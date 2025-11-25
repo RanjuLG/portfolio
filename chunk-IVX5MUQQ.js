de2d011fa3666a54b471ade5becf6fba141800fc
RanjuLG <RanjuLG@users.noreply.github.com>
2025-11-25 00:44:12 +0000

    chore(publish): deploy from GitHub Actions (${GITHUB_SHA}) 039d41372e5781a43987d20f9f728ee28d816393

---
+++ chunk-IVX5MUQQ.js
@@ -0,0 +1,1 @@
+var a={personal:{name:"Ranju Gamage",role:"Software Engineer",email:"ranjue16@gmail.com",location:"Sri Lanka"},social:{github:"https://github.com/RanjuLG",linkedin:"https://linkedin.com/in/ranju-laksahan",twitter:""},urls:{resume:"Ranju_Gamage_Resume.pdf",baseUrl:"https://ranjugamage.dev"},seo:{siteName:"Ranju Gamage - Software Engineer"}};export{a};
