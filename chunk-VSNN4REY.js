2785c703614d5ce7201d8d9281296218c0283547
RanjuLG <RanjuLG@users.noreply.github.com>
2026-01-15 20:34:28 +0000

    chore(publish): deploy from GitHub Actions (${GITHUB_SHA}) 933023bf7b434413f919cec1ba8b4a2630e70da6

---
+++ chunk-VSNN4REY.js
@@ -0,0 +1,2 @@
+import{Q as e}from"./chunk-2G6NCCEV.js";var i=(()=>{class t{constructor(){}trackEvent(r,o,n,c){console.log(`[Analytics] ${r} - ${o}: ${n}`,c?`(${c})`:"")}static \u0275fac=function(o){return new(o||t)};static \u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"})}return t})();export{i as a};
+//# sourceMappingURL=chunk-VSNN4REY.js.map
