2dd258466f5eb84be120c3bc8767c6dab67c4d9b
RanjuLG <RanjuLG@users.noreply.github.com>
2026-08-01 14:49:59 +0000

    chore(publish): deploy from GitHub Actions (${GITHUB_SHA}) ff307490ed191e4e8be3043d2fc965e80278d33a

---
+++ chunk-TM6RGFGR.js
@@ -0,0 +1,2 @@
+import{Q as e}from"./chunk-6Z4KV3A7.js";var i=(()=>{class t{constructor(){}trackEvent(r,o,n,c){console.log(`[Analytics] ${r} - ${o}: ${n}`,c?`(${c})`:"")}static \u0275fac=function(o){return new(o||t)};static \u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"})}return t})();export{i as a};
+//# sourceMappingURL=chunk-TM6RGFGR.js.map
